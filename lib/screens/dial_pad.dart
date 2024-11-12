590abe7432b5baab194ae0bcb025f082c457f15f
Rishabh Varshney <right.rishabh@gmail.com>
2024-11-12 11:45:12 +0530

    Modified dialer for web.

--- lib/screens/dial_pad.dart
+++ lib/screens/dial_pad.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:voxflut/screens/call_screen.dart';
 
@@ -79,76 +80,86 @@ class _DialPadState extends State<DialPad> {
       _enteredNumber = widget.number ?? "+91";
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "+91 ${_enteredNumber.substring(3)}",
-                style: const TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 370),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "+91 ${_enteredNumber.substring(3)}",
+                    style: const TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  GestureDetector(
+                    onTap: _onBackspace,
+                    child: const Icon(Icons.backspace, size: 25),
+                  ),
+                ],
               ),
-              const SizedBox(width: 30),
-              GestureDetector(
-                onTap: _onBackspace,
-                child: const Icon(Icons.backspace, size: 25),
-              ),
-            ],
+            ),
           ),
-        ),
-        const SizedBox(height: 50),
-        Wrap(
-          spacing: 15,
-          runSpacing: 15,
-          alignment: WrapAlignment.center,
-          children: [
-            ...List.generate(9, (index) {
-              return _buildNumberButton('${index + 1}');
-            }),
-            ElevatedButton(
-              onPressed: () => {
-                setState(() {
-                  _enteredNumber = "+91";
-                })
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red.shade400,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40),
+          const SizedBox(height: 50),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: kIsWeb ? 400 : 400),
+            child: Wrap(
+              spacing: 15,
+              runSpacing: 15,
+              alignment: WrapAlignment.center,
+              children: [
+                ...List.generate(9, (index) {
+                  return _buildNumberButton('${index + 1}');
+                }),
+                ElevatedButton(
+                  onPressed: () => {
+                    setState(() {
+                      _enteredNumber = "+91";
+                    })
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red.shade400,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    minimumSize: const Size(120, 100),
+                  ),
+                  child: const Icon(
+                    Icons.delete,
+                    size: 40,
+                    color: Colors.white,
+                  ),
                 ),
-                minimumSize: const Size(120, 100),
-              ),
-              child: const Icon(
-                Icons.delete,
-                size: 40,
-                color: Colors.white,
-              ),
-            ),
-            _buildNumberButton('0'),
-            ElevatedButton(
-              onPressed: () => _onCall(),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green.shade400,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40),
+                _buildNumberButton('0'),
+                ElevatedButton(
+                  onPressed: () => _onCall(),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green.shade400,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    minimumSize: const Size(120, 100),
+                  ),
+                  child: const Icon(
+                    Icons.call,
+                    size: 40,
+                    color: Colors.white,
+                  ),
                 ),
-                minimumSize: const Size(120, 100),
-              ),
-              child: const Icon(
-                Icons.call,
-                size: 40,
-                color: Colors.white,
-              ),
+              ],
             ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
